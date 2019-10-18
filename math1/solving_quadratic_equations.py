0149291af73ceaa35ee5b646b58cd374f50419aa
choonhao123 <ng.choonhao@dhs.sg>
2019-10-18 13:10:37 +0800

    Update solving_quadratic_equations.py

--- math1/solving_quadratic_equations.py
+++ math1/solving_quadratic_equations.py
@@ -7,9 +7,10 @@ a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
 
-if (b*b - 4*a*c < 0):
+if (b**2 - 4a*c < 0):
   print("No real roots")
-elif (b*b - 4*a*c == 0): # equal roots
-  print(-b/(2*a))
+elif (b**2 - 4a*c == 0): # equal roots
+  print(-b/(2a))
 else: # (b*b - 4*a*c > 0) i.e. distinct roots
-  print((-b+math.sqrt(b*b-4*a*c))/(2*a), (-b-math.sqrt(b*b-4*a*c))/(2*a))
+  print((-b+math.sqrt(b**2-4a*c))/(2a)) #quadratic formula
+  print((-b-math.sqrt(b**2-4a*c))/(2a))
