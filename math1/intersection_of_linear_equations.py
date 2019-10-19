10de0e032ae034b4bc9ab059e63dd1ad2ac99e02
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-19 18:12:50 +0800

    Update and rename intersection of linear eqautions to intersection_of_linear_equations.py

---
+++ math1/intersection_of_linear_equations.py
@@ -0,0 +1,13 @@
+# Intersection of linear equations
+
+a = float(input("Enter a, b for y = ax + b: "))
+b = float(input())
+c = float(input("Enter c, d for y - cx + d: "))
+d = float(input())
+
+if a == c:
+    print("No intersection")
+else:
+    m = (d - b) / (a - c)
+    n = a * m + b
+    print("coordinate of intersection is (", m, ",", n, ")")
